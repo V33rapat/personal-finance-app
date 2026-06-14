3527c5329c0ca45b8dffbd7d5de68a417a3a320e
V33rapat <veerapat.v@kkumail.com>
2026-06-14 18:39:27 +0700

    feat(transaction): implement GET endpoint for fetching transactions with walletId

--- apps/frontend/src/app/api/transaction/route.ts
+++ apps/frontend/src/app/api/transaction/route.ts
@@ -19,6 +19,34 @@ async function getAuthHeader() {
   };
 }
 
+export async function GET(request: NextRequest) {
+  const authHeader = await getAuthHeader();
+
+  if(!authHeader){
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const searchParams = request.nextUrl.searchParams;
+  const walletId = searchParams.get("walletId");
+
+  const url = new URL(`${getBackendURL()}/transaction?walletId=${walletId}`);
+  if(walletId){
+    url.searchParams.set("walletId", walletId);
+  }
+
+  const res = await fetch(url, {
+    headers: authHeader,
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    return NextResponse.json({ message: data.message }, { status: res.status });
+  }
+
+  return NextResponse.json(data, { status: res.status });
+}
+
 export async function POST(request: NextRequest) {
   const authHeader = await getAuthHeader();
 
