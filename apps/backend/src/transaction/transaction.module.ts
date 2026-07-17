f276ba24070ae5795e9d63cf249ce17bbaa78f09
V33rapat <veerapat.v@kkumail.com>
2026-07-17 23:42:05 +0700

    fix(backend): resolve JwtAuthGuard dependencies

--- apps/backend/src/transaction/transaction.module.ts
+++ apps/backend/src/transaction/transaction.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [TransactionController],
   providers: [TransactionService, PrismaService],
 })
--- apps/backend/src/transfer/transfer.module.ts
+++ apps/backend/src/transfer/transfer.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { TransferController } from './transfer.controller';
 import { TransferService } from './transfer.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [TransferController],
   providers: [TransferService, PrismaService],
 })
